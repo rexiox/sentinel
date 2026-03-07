285bfc7b5288e92a7025caa244b4e0ed37e2e682
resulsilay <resulsilay@gmail.com>
2026-03-08 00:43:56 +0300

    Feat: Check URL Schemes to Detect Jailbreak

--- sentinel-kit/detector/src/iosMain/kotlin/sentinel/kit/detector/JailbreakDetector.ios.kt
+++ sentinel-kit/detector/src/iosMain/kotlin/sentinel/kit/detector/JailbreakDetector.ios.kt
@@ -3,6 +3,8 @@ package sentinel.kit.detector
 import kotlinx.cinterop.ExperimentalForeignApi
 import platform.Foundation.NSFileManager
 import platform.Foundation.NSFileTypeSymbolicLink
+import platform.Foundation.NSURL
+import platform.UIKit.UIApplication
 import sentinel.core.detector.SecurityDetector
 import sentinel.core.detector.Threat
 import sentinel.core.violation.IosViolation
@@ -12,42 +14,39 @@ class JailbreakDetector : SecurityDetector {
 
     override fun detect(): List<Threat> = buildList {
         if (checkJailbreakApps()) {
-            add(
-                element = Threat(
-                    violation = IosViolation.Jailbreak.AppInstalled()
-                )
-            )
+            add(element = Threat(violation = IosViolation.Jailbreak.AppInstalled()))
         }
 
-        if (checkMounts()) {
-            add(
-                Threat(
-                    violation = IosViolation.Jailbreak.SuspiciousMount()
-                )
-            )
+        if (checkURLSchemes()) {
+            add(element = Threat(violation = IosViolation.Jailbreak.URLSchemes()))
+        }
+
+        if (checkSuspiciousSymlinks()) {
+            add(element = Threat(violation = IosViolation.Jailbreak.SuspiciousSymlinks()))
         }
     }
 
     private fun checkJailbreakApps(): Boolean {
         val fileManager = NSFileManager.defaultManager
 
-        return DetectorConst.JAILBREAK_APPS.any(predicate = fileManager::fileExistsAtPath)
+        return DetectorConst.APP_PATHS.any(predicate = fileManager::fileExistsAtPath)
+    }
+
+    private fun checkURLSchemes(): Boolean {
+        val sharedApp = UIApplication.sharedApplication
+
+        return DetectorConst.URL_SCHEMES
+            .mapNotNull { NSURL.URLWithString(it) }
+            .any(predicate = sharedApp::canOpenURL)
     }
 
     @OptIn(ExperimentalForeignApi::class)
-    private fun checkMounts(): Boolean {
+    private fun checkSuspiciousSymlinks(): Boolean {
         val fileManager = NSFileManager.defaultManager
 
-        for (path in DetectorConst.MOUNTS) {
-            val attributes = try {
-                fileManager.attributesOfItemAtPath(path, null)
-            } catch (_: Throwable) {
-                null
-            }
-            val type = attributes?.get("NSFileType") as? String
-            if (type == NSFileTypeSymbolicLink) return true
+        return DetectorConst.SUSPICIOUS_SYMLINKS.any { path ->
+            val attributes = fileManager.attributesOfItemAtPath(path, null)
+            attributes?.get(platform.Foundation.NSFileType) == NSFileTypeSymbolicLink
         }
-
-        return false
     }
 }
