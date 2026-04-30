493f15f2bf0155554470141f9518390f98f7bf50
resulsilay <resulsilay@gmail.com>
2026-04-30 15:49:17 +0300

    feat: implement safe config validation

--- sentinel/src/commonMain/kotlin/sentinel/Config.kt
+++ sentinel/src/commonMain/kotlin/sentinel/Config.kt
@@ -1,5 +1,7 @@
 package sentinel
 
+import sentinel.core.logger.SentinelLogger
+
 data class Config(
     var appId: List<Byte> = emptyList(),
     var appIntegrity: List<Byte> = emptyList(),
@@ -7,13 +9,19 @@ data class Config(
     var isLoggingEnabled: Boolean = false,
 ) {
 
-    internal fun validate() {
+    internal fun validate() = runCatching {
         require(appId.isNotEmpty()) {
             "Config validation failed: appId must not be empty"
+        }
+
+        require(appIntegrity.isNotEmpty()) {
+            "Config validation failed: appIntegrity must not be empty"
         }
 
         require(threshold in 0..500) {
             "Config validation failed: threshold must be between 0 and 500"
         }
+    }.onFailure { exception ->
+        SentinelLogger.print(msg = exception.message.orEmpty())
     }
 }
